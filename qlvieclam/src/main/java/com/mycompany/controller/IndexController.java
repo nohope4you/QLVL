e78c13cb91cf145eb6e328a92ae59d59848f5bcb
nohope4you <skullir4@gmail.com>
2023-08-06 18:59:39 +0700

    Lần 1

---
+++ qlvieclam/src/main/java/com/mycompany/controller/IndexController.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author Admin
+ */
+@Controller
+public class IndexController {
+    @RequestMapping("/")
+    public String index(Model model){
+        model.addAttribute("msg","My viec làm");
+        return "index";
+    }
+    
+   
+}
